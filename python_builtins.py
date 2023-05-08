506d5bc6adf6ee809c37114a27b07c7f925084ff
6lackwater <tcray.dev@gmail.com>
2023-05-08 14:31:50 -0500

    Data Types Upload

---
+++ python_builtins.py
@@ -0,0 +1,10 @@
+# Range: use in loops to iterate over a set of numbers
+for i in range(0, 5): # Translates to a collection of <0, 1, 2, 3, 4, 5>
+    print(i)
+
+
+# Enumerate: assigns an index as key to a collection of values
+collection = ['Apple', 'Orange', 'Pear']
+for index, item in enumerate(collection):
+    print(index, item)
+
